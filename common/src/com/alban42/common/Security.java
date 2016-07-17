45cdbb8a31bb0e541dfe4547c894e130d6b9d085
Alban42 <alban.menager@gmail.com>
2016-07-17 18:56:43 +0200

    Update gitignore and build.gradle files

---
+++ common/src/com/alban42/common/Security.java
@@ -0,0 +1,9 @@
+package com.alban42.common;
+
+/**
+ * Created by alban on 17/07/2016.
+ *
+ * @author alban
+ */
+public class Security {
+}
---
+++ mcp/src/MCPServer.java
@@ -0,0 +1,7 @@
+/**
+ * Created by alban on 17/07/2016.
+ *
+ * @author alban
+ */
+public class MCPServer {
+}
